2c35902e9f224c26cb1a3467d91c832427652f08
jackyan63@gmail.com <jackyan63@gmail.com>
2018-12-04 16:15:22 -0500

    8.5 recursive multiply improve

--- src/a_8_5_recursive_multiple_top_down_improve_odd.java
+++ src/a_8_5_recursive_multiple_top_down_improve_odd.java
@@ -1,4 +1,32 @@
 
 public class a_8_5_recursive_multiple_top_down_improve_odd {
+	
+	int minProduct(int a, int b) {
+		int bigger = a < b ? b : a;
+		int smaller = a < b ? a : b;
+		return minProductHelper(smaller, bigger);		
+	}
+	
+	int minProductHelper(int smaller, int bigger) {
+		if (smaller == 0) {
+			return 0;
+		}
+		else if (smaller == 1) {
+			return bigger;
+		}
+		
+		int s = smaller >> 1;
+		int halfProd = minProductHelper(s, bigger);
+		
+		if (smaller % 2 == 0) {
+			return halfProd + halfProd;
+		} else {
+			return halfProd + halfProd + bigger;
+		}
+	}
+	
+	
 
 }
+
+
