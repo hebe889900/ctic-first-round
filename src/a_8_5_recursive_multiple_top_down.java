dd9d4dac51ca52fbc5951524c1c38a6aa48630b3
jackyan63@gmail.com <jackyan63@gmail.com>
2018-12-04 16:12:14 -0500

    8.5 recursive multiply

--- src/a_8_5_recursive_multiple_top_down.java
+++ src/a_8_5_recursive_multiple_top_down.java
@@ -3,24 +3,29 @@ public class a_8_5_recursive_multiple_top_down {
 	int minProduct(int a, int b) {
 		int bigger = a < b ? b : a;
 		int smaller = a < b ? a : b;
-		return minProductHelper(smaller, bigger);
+		
+		int memo[] = new int[smaller + 1];
+		return minProduct(smaller, bigger, memo);
 	}
 	
-	int minProductHelper(int smaller, int bigger) {
+	int minProduct(int smaller, int bigger , int[] memo) {
 		if (smaller == 0) {
 			return 0;
 		}
 		else if (smaller == 1) {
 			return bigger;
 		}
-		
+		else if (memo[smaller] > 0) {
+			return memo[smaller];
+		}
 		int s = smaller >> 1;
-		int side1 = minProduct(s, bigger);
+		int side1 = minProduct(s, bigger, memo);
 		int side2 = side1;
 		if (smaller % 2 == 1) {
-			side2 = minProductHelper(smaller - s, bigger);
+			side2 = minProduct(smaller - s, bigger, memo);
 		}
 		
-		return side1 + side2;
+		memo[smaller] = side1 + side2;
+		return memo[smaller];
 	}
 }
---
+++ src/a_8_5_recursive_multiple_top_down_improve_odd.java
@@ -0,0 +1,4 @@
+
+public class a_8_5_recursive_multiple_top_down_improve_odd {
+
+}
