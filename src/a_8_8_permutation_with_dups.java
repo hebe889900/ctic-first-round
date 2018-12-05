7b07df723ca215e41e5d2357d40f7ab2e75eb454
jackyan63@gmail.com <jackyan63@gmail.com>
2018-12-05 13:28:23 -0500

    8.8 permutation with dups

---
+++ src/a_8_8_permutation_with_dups.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class a_8_8_permutation_with_dups {
+	ArrayList<String> printPerms(String s) {
+		ArrayList<String> result = new ArrayList<String>();
+		HashMap<Character, Integer> map = buildFreqTable(s);
+		printPerms(map, "", s.length(), result);
+		return result;
+	}
+	
+	HashMap<Character, Integer> buildFreqTable(String s) {
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : s.toCharArray()) {
+			if (!map.containsKey(c)) {
+				map.put(c, 0);
+			}
+			map.put(c, map.get(c) + 1);
+		}
+		return map;
+	}
+	
+	void printPerms(HashMap<Character, Integer> map, String prefix, int remaining, ArrayList<String> result) {
+		if (remaining == 0) {
+			result.add(prefix);
+			return;
+		}
+		
+		for (Character c : map.keySet()) {
+			int count = map.get(c);
+			if (count > 0) {
+				map.put(c, count - 1);
+				printPerms(map, prefix + c, remaining - 1, result);
+				map.put(c, count);
+			}
+		}
+	}
+}
