437effebb62a613d29de09a7ce541922cdf30263
jackyan63@gmail.com <jackyan63@gmail.com>
2018-12-18 12:42:21 -0500

    10.8

---
+++ src/a_10_8_find_duplicates.java
@@ -0,0 +1,34 @@
+public class a_10_8_find_duplicates {
+	void checkDuplicates(int[] array) {
+		BitSet bs = new BitSet(32000);
+		for (int i = 0; i < array.length; i++) {
+			int num = array[i];
+			int num0 = num - 1;
+			if (bs.get(num0)) {
+				System.out.println(num);
+			} else {
+				bs.set(num0);
+			}
+		}
+	}
+	
+	class BitSet {
+		int[] bitset;
+		
+		public BitSet(int size) {
+			bitset = new int[(size >> 5) + 1];
+		}
+		
+		boolean get(int pos) {
+			int wordNumber = (pos >> 5);
+			int bitNumber = (pos & 0x1F);
+			return (bitset[wordNumber] & (1 << bitNumber)) != 0;
+		}
+		
+		void set(int pos) {
+			int wordNumber = (pos >> 5);
+			int bitNumber = (pos & 0x1F);
+			bitset[wordNumber] = 1 << bitNumber;
+		}
+	}
+}
