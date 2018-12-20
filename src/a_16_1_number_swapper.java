e2cfc7549531260d0dcb377792862b3a5ce3e80e
jackyan63@gmail.com <jackyan63@gmail.com>
2018-12-20 12:39:14 -0500

    16.1

--- src/a_16_1_number_swapper.java
+++ src/a_16_1_number_swapper.java
@@ -1,4 +1,14 @@
 
 public class a_16_1_number_swapper {
-
+	void swap(int a, int b) {
+		a = a - b;
+		b = a + b;
+		a = b - a;
+	}
+	
+	void swap_bit(int a, int b) {
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+	}
 }
